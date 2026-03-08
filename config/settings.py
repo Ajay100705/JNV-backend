f6a6ca1e7cf0190fd8da8612cf059904a50cb4eb
Ajay Chandel <chandelajay2812@gmail.com>
2026-03-09 04:29:43 +0530

    fix database config

--- config/settings.py
+++ config/settings.py
@@ -102,12 +102,24 @@ WSGI_APPLICATION = 'config.wsgi.application'
 #     )
 # }
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600
-    )
-}
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL:
+    # Used on Render (PostgreSQL)
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600
+        )
+    }
+else:
+    # Used locally (SQLite)
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
@@ -160,11 +172,11 @@ SIMPLE_JWT = {
 }
 
 
-CORS_ALLOW_ALL_ORIGINS = [
-    "http://localhost:3000",
-    "http://localhost:5173",
-    "https://jnv-frontend.vercel.app"
-]   
+# CORS_ALLOW_ALL_ORIGINS = [
+#     "http://localhost:3000",
+#     "http://localhost:5173",
+#     "https://jnv-frontend.vercel.app"
+# ]   
 CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = [
