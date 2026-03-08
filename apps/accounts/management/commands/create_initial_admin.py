2afeb8c9b9d84a1828c746800930c3267ff9b7e1
Ajay Chandel <chandelajay2812@gmail.com>
2026-03-09 03:09:00 +0530

    create initial principal superuser

---
+++ apps/accounts/management/commands/create_initial_admin.py
@@ -0,0 +1,25 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Command(BaseCommand):
+    help = "Create initial principal superuser"
+
+    def handle(self, *args, **kwargs):
+        username = "principal"
+        password = "principal123"
+
+        if not User.objects.filter(username=username).exists():
+            user = User.objects.create_superuser(
+                username=username,
+                password=password,
+                email="principal@school.com"
+            )
+
+            user.role = "principal"
+            user.save()
+
+            self.stdout.write(self.style.SUCCESS("Principal superuser created"))
+        else:
+            self.stdout.write("Principal already exists")
