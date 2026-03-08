7d1c9b2aaf5b28495aced580b936ed332b7d860d
Ajay Chandel <chandelajay2812@gmail.com>
2026-03-09 02:18:40 +0530

    chande settings, for deployment

--- config/settings.py
+++ config/settings.py
@@ -95,9 +95,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default='sqlite:///db.sqlite3',
+#         conn_max_age=600
+#     )
+# }
+
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
+        default=os.environ.get("DATABASE_URL"),
         conn_max_age=600
     )
 }
